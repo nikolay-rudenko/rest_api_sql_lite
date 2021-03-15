72cb18d90f404abc932f3ba4f687f95a633a7ef4
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-03-15 21:24:11 +0200

    additional changes

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=./code/rest_api_sql_lite.py
+FLASK_ENV=development
--- code/user.py
+++ code/user.py
@@ -1,5 +1,44 @@
+import sqlite3
+
+
 class User:
     def __init__(self, _id, user_name, password):
         self.id = _id
         self.user_name = user_name
         self.password = password
+
+    @classmethod
+    def find_by_user_name(cls, user_name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM users WHERE user_name=?'
+        result = cursor.execute(query, (user_name,))
+
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_user_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM users WHERE id=?'
+        result = cursor.execute(query, (_id,))
+
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+
